49c985ec24e31a56766860b9cdb8ab413989bbc4
RoidoCTB <lloydhhannessanchez@gmail.com>
2022-11-08 20:33:47 +0800

    change html to php

--- login.php
+++ login.php
@@ -33,7 +33,7 @@
                     </form>
                   </div>
                   <div>
-                    <p class="mb-0">Don't have an account? <a href="signup.html" class="text-white-50 fw-bold">Sign Up</a>
+                    <p class="mb-0">Don't have an account? <a href="signup.php" class="text-white-50 fw-bold">Sign Up</a>
                     </p>
                   </div>
                 </div>
